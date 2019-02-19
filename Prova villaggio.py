eae16b6226b06336f694690cd0d6e7007d7eef23
liberodaniele <maggioli.daniele@gmail.com>
2019-02-19 22:19:25 +0100

    Update Prova villaggio.py

--- Prova villaggio.py
+++ Prova villaggio.py
@@ -3,22 +3,29 @@ class Building:
     tipo=0 #1=HQ, 2=TC, 3=CP, 4=IM
     production=[0,0,0]
     population=0
-    cost=[0,0,0,]
+    cost=[0,0,0]
+    time=0
 
     def __init__(self,n):
         tipo=n
+
+    def upgrade(self):
+        self.lvl=self.lvl+1
         
 class Villaggio :
-    
+
+    resources=[100,1,1]
+    population=0
     hq=Building(1)
     tc=Building(2)
     cp=Building(3)
     
     def __init__(self):
-        self.hq.lvl=1
+        self.hq.lvl=0
 
     def build(self, obj):
-        obj.lvl=obj.lvl+1
+        if obj.cost[0]<self.resources[0] & obj.cost[1]<self.resources[1] & obj.cost[2]<self.resources[2]:
+            obj.upgrade()
 
     def livello(self):
         return self.hq.lvl
